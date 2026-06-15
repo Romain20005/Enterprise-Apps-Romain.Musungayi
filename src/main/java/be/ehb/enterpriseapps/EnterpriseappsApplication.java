2ff34711cb652eaf316fbfcb46ee76609386355b
Romain Musungayi <jouwgithubemail@gmail.com>
2026-06-15 19:20:37 +0200

    Initial Spring Boot project setup

---
+++ src/main/java/be/ehb/enterpriseapps/EnterpriseappsApplication.java
@@ -0,0 +1,13 @@
+package be.ehb.enterpriseapps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseappsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterpriseappsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/ehb/enterpriseapps/EnterpriseappsApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ehb.enterpriseapps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseappsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
